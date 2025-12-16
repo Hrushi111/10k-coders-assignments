52fe481b270026930118060b4cf77481a65f69a8
Hrushi111 <123741337+Hrushi111@users.noreply.github.com>
2025-12-16 16:50:09 +0530

    Add files via upload

---
+++ tasks for git/Printing_Variables.java
@@ -0,0 +1,7 @@
+public class Printing_Variables{
+	public static void main(String [] args){
+		int a =10;
+		String b ="Hrushi";
+		Boolean c= false;
+		float f = 22.222f;
+		System.out.println("\na is int : "+ a+"\nb is String :"+b +"\nc is Boolean : " + c +" \nf is float: "+f);}}
