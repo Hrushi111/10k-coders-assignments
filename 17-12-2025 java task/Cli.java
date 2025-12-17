e9990e691897c336ea44dabc9cb78c9963df06a2
Hrushi111 <123741337+Hrushi111@users.noreply.github.com>
2025-12-17 11:41:48 +0530

    Add files via upload

---
+++ 17-12-2025 java task/Cli.java
@@ -0,0 +1,7 @@
+public class Cli{
+	public static void main(String ... args){
+		System.out.println(" Canteen items ");
+		System.out.println(" 1." + args[0]);
+		System.out.println(" 2." + args[1]);
+		System.out.println(" 3." + args[2]);
+}}
